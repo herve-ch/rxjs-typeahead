354e080678227e5ceafa4958002a22da9a046458
herve-ch <rv_changue@hotmail.com>
2023-01-02 21:48:26 -1000

    rxjs sample

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+import { debounceTime, distinctUntilChanged, fromEvent, map, switchMap } from "rxjs";
+import { ajax } from 'rxjs/ajax';
+
 const input = document.querySelector("input");
 const resultats = document.querySelector(".resultats");
 
@@ -5,24 +8,26 @@ const resultats = document.querySelector(".resultats");
  * Le résultat attendu via l'utilisation d'RxJS
  */
 
-// const frappes$ = fromEvent(input, "input");
+const frappes$ = fromEvent(input, "input");
 
-// frappes$
-//   .pipe(
-//     map((event) => event.target.value),
-//     distinctUntilChanged(),
-//     debounceTime(500),
-//     switchMap((text) => ajax("https://geo.api.gouv.fr/communes?nom=" + text)),
-//     map((resultats) => resultats.response)
-//   )
-//   .subscribe((communes) => {
-//     let communesHtml = communes
-//       .map((commune) => `<li>${commune.nom} (${commune.code})</li>`)
-//       .join("");
-//     resultats.innerHTML = `
-//         <h2>Résultats : </h2>
-//         <ul>
-//             ${communesHtml}
-//         </ul>
-//     `;
-//   });
+frappes$
+  .pipe(
+    map((event) => event.target.value),
+    distinctUntilChanged(),
+    debounceTime(500),
+    switchMap((text) => ajax("https://geo.api.gouv.fr/communes?nom=" + text)),
+    map((resultats) => resultats.response)
+  )
+  .subscribe((communes) => {
+    let communesHtml = communes
+      .map((commune) => `<li>${commune.nom} (${commune.code})</li>`)
+      .join("");
+    resultats.innerHTML = `
+        <h2>Résultats : </h2>
+        <ul>
+            ${communesHtml}
+        </ul>
+    `;
+  });
+
+  
